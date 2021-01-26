b7ad53ba2c3afceae2b836f4783d40c621f901f4
Sergio <scarolayton@gmail.com>
2021-01-26 08:32:29 -0500

    arreglando problema menu responsive

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 const hamburger = document.querySelector(".btn-menu");
 const menu = document.querySelector(".nav-menu");
-
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("showMenu");
-   
-  if(menu.className === "nav-menu showMenu"){
+  if (menu.className === "nav-menu showMenu") {
     document.getElementsByTagName("html")[0].style.overflow = "hidden";
-    document.getElementsByTagName("body")[0].style.backgroundColor = "rgba(44, 39, 39, 0.938)";
-  }else {
+    document.getElementsByTagName("body")[0].style.backgroundColor =
+      "rgba(44, 39, 39, 0.938)";
+  } else {
     document.getElementsByTagName("html")[0].style.overflow = "auto";
-    document.getElementsByTagName("body")[0].style.backgroundColor = "transparent";
+    document.getElementsByTagName("body")[0].style.backgroundColor =
+      "transparent";
   }
-  
+});
+
+menu.childNodes[1].addEventListener("click", () => {
+  document.getElementsByTagName("html")[0].style.overflow = "auto";
+  document.getElementsByTagName("body")[0].style.backgroundColor =
+    "transparent";
+  menu.classList.remove("showMenu");
 });
 //Scroll Efect
 
